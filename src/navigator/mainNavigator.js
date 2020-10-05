6cfc4ddf1af3c3064432e99c4584f636e7d228d2
Crowdbotics <team@crowdbotics.com>
2020-10-05 04:12:22 +0000

    add package blueprint-react-native-maps-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Maps126652Navigator from '../features/Maps126652/navigator';
 import UserProfile126648Navigator from '../features/UserProfile126648/navigator';
 import Settings126611Navigator from '../features/Settings126611/navigator';
 import Settings126594Navigator from '../features/Settings126594/navigator';
@@ -16,6 +17,7 @@ import Settings126594Navigator from '../features/Settings126594/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Maps126652: { screen: Maps126652Navigator },
 UserProfile126648: { screen: UserProfile126648Navigator },
 Settings126611: { screen: Settings126611Navigator },
 Settings126594: { screen: Settings126594Navigator },
